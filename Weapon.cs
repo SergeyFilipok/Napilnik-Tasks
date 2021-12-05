1ec1d3ff9bf98453c0a7ba10d111e6f7280c82e4
Sergei Filimonov <sergey@armadillo.games>
2021-12-05 12:14:38 +0300

    Fix code

--- Weapon.cs
+++ Weapon.cs
@@ -12,25 +12,25 @@ public class Weapon {
     }
 
     public void Fire(IDamageable damageable) {
-        if (Bullets > 0) {
-            damageable.TakeDamage(Damage);
-            Bullets -= 1;
-        }
-        else {
+        if (HasBulletsInClip == false) {
             throw new InvalidOperationException("No bullets!");
         }
+
+        damageable.TakeDamage(Damage);
+        Bullets -= 1;
     }
 }
 
 public class Player : IDamageable {
     public int Health { get; private set; }
-    public bool IsDead => Health <= 0;
     public bool IsAlive => Health > 0;
 
     public void TakeDamage(int damage) {
-        if (IsAlive && damage > 0) {
-            Health -= damage;
+        if (IsAlive == false) {
+            throw new InvalidOperationException("This player is dead!");
         }
+
+        Health -= damage;
 
         if (Health < 0) {
             Health = 0;
@@ -42,25 +42,11 @@ public class Bot {
     private Weapon _weapon;
 
     public Bot(Weapon weapon) {
-        Weapon = weapon;
-    }
-
-    public bool HasWeapon => Weapon != null;
-
-    public Weapon Weapon {
-        get => _weapon;
-        set {
-            _weapon = value;
-            if (_weapon == null) {
-                throw new NullReferenceException(nameof(_weapon));
-            }
-        }
+        _weapon = weapon ?? throw new NullReferenceException("Weapon in NULL");
     }
 
     public void OnSeePlayer(Player player) {
-        if (HasWeapon && player.IsAlive) {
-            Weapon.Fire(player);
-        }
+        _weapon.Fire(player);
     }
 }
 
